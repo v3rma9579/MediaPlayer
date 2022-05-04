c917dd5a3d00148f16981c00adf9a581d585d800
Bivas Kumar <44bivaskumar29u@gmail.com>
2022-05-04 17:16:50 +0530

    fixed build automation

--- scripts/build-exe.py
+++ scripts/build-exe.py
@@ -4,13 +4,13 @@ import shutil
 import PyInstaller.__main__
 
 if __name__ == '__main__':
-    APP_NAME = r'Media Player'
+    APP_NAME = r'Simple Media Player'
     # APP_DIR aka ROOT DIR of the project
     APP_DIR = pathlib.PurePath(__file__).parents[1]
     # path to the main.py file
     APP_PATH = APP_DIR.joinpath('sources', 'main.py')
-    ICON_PATH = APP_DIR.joinpath('icons', '256x256.ico')
-    ICON_DIR = APP_DIR.joinpath('icons')
+    ASSETS_DIR = APP_DIR.joinpath('assets')
+    ICON_PATH = APP_DIR.joinpath(ASSETS_DIR, 'smp.ico')
     DIST_DIR = pathlib.Path(APP_DIR.joinpath('release'))
     try:
         DIST_DIR.mkdir(exist_ok=True)
@@ -23,7 +23,8 @@ if __name__ == '__main__':
             '--distpath',
             fr'{DIST_DIR}',
             '--add-data',
-            f'{ICON_DIR};icons',
+            f'{ASSETS_DIR};ASSETS',
+            '--onedir',
             '--clean',
             '--noconsole',
             '--noconfirm'
